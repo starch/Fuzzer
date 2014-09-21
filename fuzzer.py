2f4c8365a21cfd22f9896b34f6b50189f4e0e8f9
Tyler Schoen <tyler@ethode.com>
2014-09-21 16:19:49 -0400

    Added sessions over requests to maintain all cookies over the fuzzing of a domain. Also added complete query string parsing and finished adding the necessary global variables to manage the links once they are processed.

--- fuzzer.py
+++ fuzzer.py
@@ -1,11 +1,13 @@
 import sys
 import urllib2
 import requests
-from requests.exceptions import ConnectionError, MissingSchema
-from urllib.parse import urlparse
+from urlparse import urlparse
+from requests.exceptions import ConnectionError, MissingSchema, ReadTimeout
 
 #DATA STRUCTURES
+fuzzerSession = ''
 queryStrings = []
+links = []
 
 #GLOBAL SETTINGS
 mode = ''
@@ -14,10 +16,13 @@ vectors = []
 sensitive = []
 random = 0
 slow = 500
-pageExtensions = ['.html', '.aspx', '.jsp', '.php', '.asp']
+pageExtensions = ['.html', '.aspx', '.jsp', '.jspx', '.php', '.asp', '.htm', '.do', '.rb', '.rhtml']
+commonWords = []
 
 def main():
 	print('Fuzzer has started!')
+	global domain
+	global fuzzerSession
 
 	if sys.argv.__len__() > 2 and sys.argv.__len__() <= 5:
 		if sys.argv[1].lower() == 'discover':
@@ -28,10 +33,12 @@ def main():
 		domain = sys.argv[2].lower()
 
 		try:
-			r = requests.get(domain, timeout=3)
+			r = requests.get(domain, timeout=10)
 
 			if r.status_code == 200:
 				print(domain + ' is a valid URL')
+				s = requests.Session()
+				fuzzerSession = s.get(domain)
 
 			if mode == 'discover':
 				#Call discover function here
@@ -43,80 +50,108 @@ def main():
    			print(domain + ' is not responding')
    		except MissingSchema as m:
    			print(domain + ' is not a valid URL')
+   		except ReadTimeout as t:
+   			print('Request to ' + domain + ' timed out')
 
 	else:
 		print('Please enter a fuzzer mode followed by a domain')
 
 def discoverHelper():
+	global domain
+	global fuzzerSession
+	global commonWords
 
 	for x in range(3, sys.argv.__len__()):
 		if '--common-words=' in sys.argv[x]:
 			filePath = sys.argv[x][15:]
 			wordFile = open(filePath)
-			commonWords = []
+			
 
 			for line in wordFile:
 				commonWords.append(line)
 			wordFile.close()
-
-			for word in commonWords:
-				if domain[-1] == '/':
-
-					for ext in pageExtensions:
-
-						urlGuess = domain + word + ext
-
-						try:
-							r = requests.get(urlGuess, timeout=3)
-
-							if r.status_code == 200:
-								#Add to link array
-								pass
-						except ConnectionError as e:    
-							pass
-				   		except MissingSchema as m:
-				   			pass
-			   	else:
-
-			   		for ext in pageExtensions:
-
-				   		urlGuess = domain + '/' + word + ext
-				   		
-				   		try:
-							
-							r = requests.get(urlGuess, timeout=3)
-
-							if r.status_code == 200:
-								#Add to link array
-								pass
-						except ConnectionError as e:    
-							pass
-				   		except MissingSchema as m:
-				   			pass
 
 		if '--custom-auth=' in sys.argv[x]:
 			authString = sys.argv[x][14:]
 			authString = authString.split(',')
 			authUser = authString[0]
 			authPass = authString[1]
-			login = requests.get(domain, auth=(authUser, authPass))
+			s = requests.Session()
+			s.auth = (authUser, authPass)
+			fuzzerSession = s.get(domain)
+
+	guessPages()
+
+	cookieFinder(fuzzerSession)
+
+	for link in links:
+		parseUrlForInput(link)
+
+	for query in queryStrings:
+		print(query)
 
 
-def cookieFinder(url):
-	req = requests.get(url)
-	cookies = req.cookies
-	if cookies.__len__ < 1:
+def cookieFinder(sess):
+	cookies = sess.cookies
+	if cookies.__len__() < 1:
 		print('No cookies')
 	else:
 		for c in cookies:
-			print(c + '\n')
+			print(c)
 
 def parseUrlForInput(url):
 	result = urlparse(url)
 	#should be in the form: query=something
 	queryString = result.query
-	qStr = queryString.partition('=')
-	#should return the "query" component
-	queryStrings.append(qStr[0])
+	if queryString != '':
+		queryAry = queryString.split('&')
+		for q in queryAry:
+			qStr = q.partition('=')
+			#should return the "query" component
+			queryStrings.append(qStr[0])
+		
+	else:
+		print("No query inputs")
+
+def guessPages():
+	global links
+	global commonWords
+
+	for word in commonWords:
+		if domain[-1] == '/':
+
+			for ext in pageExtensions:
+
+				urlGuess = domain + word + ext
+
+				try:
+					r = requests.get(urlGuess, timeout=3)
+
+					if r.status_code == 200:
+						links.append(urlGuess)
+				except ConnectionError as e:    
+					pass
+		   		except MissingSchema as m:
+		   			pass
+		   		except ReadTimeout as t:
+		   			pass
+	   	else:
+
+	   		for ext in pageExtensions:
+
+		   		urlGuess = domain + '/' + word + ext
+		   		
+		   		try:
+					
+					r = requests.get(urlGuess, timeout=3)
+
+					if r.status_code == 200:
+						links.append(urlGuess)
+				except ConnectionError as e:    
+					pass
+		   		except MissingSchema as m:
+		   			pass
+		   		except ReadTimeout as t:
+		   			pass
 
 main()
